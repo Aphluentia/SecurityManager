41a74676fb63eeae967abdc2d310238e950ca502
Pedro Marques <pedroagoncalvesmarques@ua.pt>
2023-06-07 11:21:29 +0100

    Adds SecurityManager

---
+++ SecurityManager/Configurations/SessionConfigSection.cs
@@ -0,0 +1,11 @@
+﻿namespace SecurityManager.Configurations
+{
+    public class SessionConfigSection
+    {
+        public int SessionValidityInMinutes { get; set; }
+        public int KeepAliveValidityInMinutes { get; set; }
+        public string JWTSecret { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+    }
+}
---
+++ SecurityManager/Models/Entities/SecurityDataDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SecurityManager.Models.Entities
+{
+    public class SecurityDataDto
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public int PermissionLevel { get; set; }
+        public Guid WebPlatformId { get; set; }
+    }
+}
